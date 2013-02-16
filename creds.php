0eee20eba1c0ba1296a7a03f1cb7d3c42d352818
Zackary Corbett <Zyber17@gmail.com>
2013-02-16 18:53:16 -0500

    Reverted to more informational form, better indention.

--- creds.php
+++ creds.php
@@ -1,7 +1,7 @@
 <?php
-	$id = array('S3' => array(
-							'access' => '',
-							'secret' => ''
-						), 
-				'key' => '');
+    $id = array('S3' => array(
+                            'access' => 'your AWS access key here',
+                            'secret' => 'your AWS secret key here'
+                        ), 
+                'key' => 'a random alphanumeric string here');
 ?>
