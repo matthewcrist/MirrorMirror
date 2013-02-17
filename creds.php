a9abafa8bea6babf361631d8bf4aee0d5d87999f
Zackary Corbett <Zyber17@gmail.com>
2013-02-16 20:04:17 -0500

    Oops, forgot an array item for the bucket.

--- creds.php
+++ creds.php
@@ -1,7 +1,8 @@
 <?php
     $id = array('S3' => array(
                             'access' => 'your AWS access key here',
-                            'secret' => 'your AWS secret key here'
+                            'secret' => 'your AWS secret key here',
+                            'bucket' => 'the S3 bucket you want your images to be saved to'
                         ), 
                 'key' => 'a random alphanumeric string here');
 ?>
