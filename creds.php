53b07f8333f001ca53c435115a03628b31e29806
Zackary Corbett <Zyber17@gmail.com>
2013-02-26 19:18:13 -0500

    Added DB creeds into the array

--- creds.php
+++ creds.php
@@ -1,8 +1,16 @@
 <?php
-    $id = array('S3' => array(
-                            'access' => 'your AWS access key here',
-                            'secret' => 'your AWS secret key here',
-                            'bucket' => 'the S3 bucket you want your images to be saved to'
-                        ), 
-                'key' => 'a random alphanumeric string here');
+    $id = array('S3' => array('access' => 'your AWS access key here',
+                              'secret' => 'your AWS secret key here',
+                              'bucket' => 'the S3 bucket you want your images to be saved to'
+                        ),
+
+                'key' => 'a random alphanumeric string here',
+
+                'DB' => array(
+                    'db' => 'database here',
+                    'host' => 'database host here',
+                    'user' => 'database username here',
+                    'pass' => 'database password here'
+                 )
+            );
 ?>
